6fb1a1f034e9fefbd24b5971328b171434c7847c
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-02-22 08:38:05 +0100

    to-adj works

--- graph-adj.lisp
+++ graph-adj.lisp
@@ -3,5 +3,17 @@
 (in-package :graph-adj)
 
 (defun to-adj (graph)
-  (let ((order (length (graph:nodes graph))))
-    (make-array (list order order) :element-type 'bit)))
+  (let* ((nodes (graph:nodes graph))
+         (order (length nodes)))
+    (let ((matrix (make-array (list order order) :element-type 'bit)))
+      (labels ((set-edge (edge)
+                 (destructuring-bind (x y) edge
+                   (setf (aref matrix
+                               (position x nodes)
+                               (position y nodes))
+                         1))))
+        (dolist (edge (graph:edges graph))
+          (set-edge edge)
+          (unless (typep graph 'graph:digraph)
+            (set-edge (reverse edge)))))
+      matrix)))
--- test/test.lisp
+++ test/test.lisp
@@ -4,16 +4,64 @@
 
 (defsuite* :graph-adj-test)
 
-(deftest dummy
-  (is (= 1 1)))
-
 (deftest test.1
   (is (equalp #2A((0 0)
                   (0 0))
-              (to-adj (populate (make-instance 'graph) :nodes '(a b))))))
+              (to-adj (populate (make-instance 'digraph) :nodes '(a b))))))
 
 (deftest test.2
   (is (equalp #2A((0 0 0)
                   (0 0 0)
                   (0 0 0))
+              (to-adj (populate (make-instance 'digraph) :nodes '(a b c))))))
+
+(deftest test.3
+  (is (equalp #2A((0 1 0)
+                  (0 0 0)
+                  (0 0 0))
+              (to-adj (populate (make-instance 'digraph)
+                                :nodes '(a b c)
+                                :edges '((a b)))))))
+
+(deftest test.4
+  (is (equalp #2A((0 1 0)
+                  (0 0 0)
+                  (0 0 1))
+              (to-adj (populate (make-instance 'digraph)
+                                :nodes '(a b c)
+                                :edges '((a b) (c c)))))))
+
+(deftest test.5
+  (is (equalp #2A((0 0)
+                  (0 0))
+              (to-adj (populate (make-instance 'graph) :nodes '(a b))))))
+
+(deftest test.6
+  (is (equalp #2A((0 0 0)
+                  (0 0 0)
+                  (0 0 0))
               (to-adj (populate (make-instance 'graph) :nodes '(a b c))))))
+
+(deftest test.7
+  (is (equalp #2A((0 1 0)
+                  (1 0 0)
+                  (0 0 0))
+              (to-adj (populate (make-instance 'graph)
+                                :nodes '(a b c)
+                                :edges '((a b)))))))
+
+(deftest test.8
+  (is (equalp #2A((0 1 0)
+                  (1 0 0)
+                  (0 0 1))
+              (to-adj (populate (make-instance 'graph)
+                                :nodes '(a b c)
+                                :edges '((a b) (c c)))))))
+
+(deftest test.9
+  (is (equalp #2A((0 1 1)
+                  (0 0 0)
+                  (1 0 0))
+              (to-adj (populate (make-instance 'digraph)
+                                :nodes '(a b c)
+                                :edges '((a b) (c a) (a c)))))))
